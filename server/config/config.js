e4e5808fb636b34c5d6815c2be075220848d6b3e
Leonardo <leoboyer@gmail.com>
2019-05-24 15:49:27 -0400

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,29 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+// ============================
+//  Entorno
+// ============================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+
+// ============================
+//  Base de datos
+// ============================
+
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://escuela_online:upernikao19!@ds261486.mlab.com:61486/escuela_online';
+}
+
+process.env.URLDB = urlDB;
